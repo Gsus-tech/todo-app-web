ba132f7bf79f971cd2c9e3437f3070f5af6c78b2
gsus-tech <chucho.moralitos0106@gmail.com>
2025-03-19 09:57:44 -0700

    Style update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 export default function Footer(){
     return(
-        <header className="bg-indigo-600 text-white p-4">
+        <header className="bg-gray-800 text-white p-4">
             <h1 className="text-3xl font-bold text-center">Kanban To-Do List</h1>
         </header>
     );
