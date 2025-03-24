076d120582ed2cc5816b2d340f491b1d5a7bb6d6
gsus-tech <chucho.moralitos0106@gmail.com>
2025-03-24 16:04:01 -0700

    Change title name

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return(
         <header className="bg-gray-800 text-white p-4">
-            <h1 className="text-3xl font-bold text-center">Kanban To-Do List</h1>
+            <h1 className="text-3xl font-bold text-center">To-Do Web Application</h1>
         </header>
     );
 }
