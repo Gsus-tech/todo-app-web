a56d7e09428e30023255a1ebb2cc43f0f944891d
gsus-tech <chucho.moralitos0106@gmail.com>
2025-03-25 10:29:55 -0700

    Background for the login form

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
     theme: {
-    extend: {},
+      extend: {
+        backgroundImage: {
+          'soft-pattern': `linear-gradient(135deg, #f3f4f6 25%, transparent 25%, transparent 50%, #f3f4f6 50%, #f3f4f6 75%, transparent 75%, transparent),
+                           linear-gradient(45deg, #1F2937 25%, transparent 25%, transparent 75%, #1F2937 75%, #1F2937)`
+        },
+        backgroundSize: {
+          'pattern': '50px 50px'
+        },
+        backgroundPosition: {
+          'pattern': '0 0, 20px 20px'
+        }
+      },
   },
   plugins: [],
 }
