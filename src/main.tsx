4566f1d131d06fde80d24cb9b0f3c36a1a4a4729
gsus-tech <chucho.moralitos0106@gmail.com>
2025-03-25 10:30:42 -0700

    Updated routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,26 @@
 // main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
 import NotFound from './components/404';
+import Login from './components/login';
+import Signup from './components/signup';
 import './index.css';
+
+const isAuthenticated = () => {
+  const token = localStorage.getItem('token');
+  return !!token;
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+      <Route path="/" element={isAuthenticated() ? 
+          <App /> : <Navigate to="/login" />}  />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<Signup />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
